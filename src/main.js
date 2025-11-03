a54d7af912d9eb96591e62899c715e7089a07746
Juliana Braga <jujbraga@gmail.com>
2025-11-03 11:48:47 +0100

    add css modal to separated file

--- src/main.js
+++ src/main.js
@@ -6,4 +6,9 @@ import "./libs/dayjs.js";
 // CSS
 import "./styles/global.css";
 import "./styles/schedule.css";
+import "./styles/modal.css";
 import "./styles/form.css";
+
+// JS
+import "./modules/page-load.js";
+import "./modules/schedules/load.js";
