e0b8547c908aaaca2ed8eb44f186ef8f903511f5
Juliana Braga <jujbraga@gmail.com>
2025-11-10 10:29:25 +0100

    add focus to input for better accessibility

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -1,11 +1,13 @@
 const modal = document.getElementById("new-appointment-modal");
 const openModal = document.getElementById("open-modal");
 const closeModal = document.querySelector(".close");
+const input = document.getElementById("client-name");
 
 export function openAndCloseModal() {
   // Open the modal
   openModal.onclick = function () {
     modal.style.display = "block";
+    input.focus();
   };
 
   // Close the modal when the user clicks anywhere outside of it or in the close button
